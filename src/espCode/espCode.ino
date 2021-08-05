2bae85b7a20a587cd9cd8e64d3e977002672ba02
Sajiv <sajiv.shah@gmail.com>
2021-08-04 19:15:21 -0700

    Axis 4 Servo Control

--- src/espCode/espCode.ino
+++ src/espCode/espCode.ino
@@ -1,7 +1,9 @@
 #include <AccelStepper.h>
+#include <ESP32Servo.h>
 
 AccelStepper MOT_5(AccelStepper::DRIVER, 12, 13);
 AccelStepper MOT_6(AccelStepper::DRIVER, 27, 14);
+Servo MOT_4;
 
 //Stepper Maxes
 #define  Stepper_MAX_SPEED 4000
@@ -17,23 +19,30 @@ double zero_DOF4 = 0;
 double zero_DOF5 = 0;
 double zero_DOF6 = 0;
 
+double SPEED_4 = 0;
 double SPEED_5 = 0;
 double SPEED_6 = 0;
 
 //-----------------
 void setup() {
   Serial.begin(9600);
-  //Stepper
+
+  MOT_4.attach(18);
   MOT_5.setMaxSpeed(4000.0);
   MOT_6.setMaxSpeed(4000.0);
   // encoder pins
   pinMode(PIN_ENC_DOF4, INPUT);
   pinMode(PIN_ENC_DOF5, INPUT);
   pinMode(PIN_ENC_DOF6, INPUT);
+
+  setSpeed_4(0.0);
+  setSpeed_5(0.0);
+  setSpeed_6(0.0);
 }
 
 //------------------
 void loop() {
+  MOT_4.write(SPEED_4);
   MOT_6.setSpeed(SPEED_6);
   MOT_5.setSpeed(SPEED_5);
   MOT_6.runSpeed();
@@ -41,10 +50,10 @@ void loop() {
   if(Serial.available()){
     char input = Serial.read();
     if(input == '1'){
-      setSpeed_6(0.2);
+      setSpeed_4(0.2);
     }
     else if(input == '2'){
-      setSpeed_5(-0.05);
+      setSpeed_4(-0.05);
     }
     //Encoder testing
     if(input == '3'){
@@ -62,6 +71,10 @@ void loop() {
     //Stepper Testing
   }
 
+}
+
+void setSpeed_4(double speed){
+  SPEED_4 = (speed*90) + 90;
 }
 
 void setSpeed_5(double speed){
